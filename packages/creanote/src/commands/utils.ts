62a1ecd7dd31850e073b35ec757c23b4d79e01a2
elitalpa <eli@eliapps.com>
2025-06-21 19:31:53 +0200

    fix: replacing function with date-fns getISOWeek as it's more reliable

--- packages/creanote/src/commands/utils.ts
+++ packages/creanote/src/commands/utils.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { getISOWeek } from "date-fns";
 
 export function copyTemplate(templatePath: string, targetPath: string) {
   if (!fs.existsSync(templatePath)) {
@@ -12,39 +13,42 @@ export function copyTemplate(templatePath: string, targetPath: string) {
 }
 
 export function getWeekNumber(date: Date): number {
-  const weekday = {
-    sunday: 0,
-    monday: 1,
-    tuesday: 2,
-    wednesday: 3,
-    thursday: 4,
-    friday: 5,
-    saturday: 6,
-  };
-
-  // get current week thursday
-  const day = (date.getDay() + 6) % 7;
-  const thursday = new Date(date);
-  thursday.setDate(date.getDate() - day + (7 - weekday.thursday));
-
-  // get first thursday of the year
-  const firstThursday = new Date(thursday.getFullYear(), 0, 1);
-  if (firstThursday.getDay() !== weekday.thursday) {
-    firstThursday.setMonth(
-      0,
-      1 + ((weekday.thursday + 7 - firstThursday.getDay()) % 7)
-    );
-  }
-
-  // calculate the number of weeks between
-  const diff = Number(thursday) - Number(firstThursday);
-  const oneDay = 1000 * 60 * 60 * 24;
-  const oneWeek = oneDay * 7;
-
-  const weekNum = 1 + Math.floor(diff / oneWeek);
-
-  return weekNum;
+  return getISOWeek(date);
 }
+// export function getWeekNumber(date: Date): number {
+//   const weekday = {
+//     sunday: 0,
+//     monday: 1,
+//     tuesday: 2,
+//     wednesday: 3,
+//     thursday: 4,
+//     friday: 5,
+//     saturday: 6,
+//   };
+
+//   // get current week thursday
+//   const day = (date.getDay() + 6) % 7;
+//   const thursday = new Date(date);
+//   thursday.setDate(date.getDate() - day + (7 - weekday.thursday));
+
+//   // get first thursday of the year
+//   const firstThursday = new Date(thursday.getFullYear(), 0, 1);
+//   if (firstThursday.getDay() !== weekday.thursday) {
+//     firstThursday.setMonth(
+//       0,
+//       1 + ((weekday.thursday + 7 - firstThursday.getDay()) % 7)
+//     );
+//   }
+
+//   // calculate the number of weeks between
+//   const diff = Number(thursday) - Number(firstThursday);
+//   const oneDay = 1000 * 60 * 60 * 24;
+//   const oneWeek = oneDay * 7;
+
+//   const weekNum = 1 + Math.floor(diff / oneWeek);
+
+//   return weekNum;
+// }
 
 export function replaceTemplateVariables(
   template: string,
